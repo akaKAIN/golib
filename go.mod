36b97df54bd710fb89db5af3ce0322429d889962
Mikhail Mir <mirzaakhmetov@infodev.ru>
2021-02-25 15:54:19 +1000

    added logrus as modul requirement

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/akaKAIN/golib
+
+go 1.15
+
+require github.com/sirupsen/logrus v1.8.0
--- mylib/mylib.go
+++ mylib/mylib.go
@@ -1,11 +1,11 @@
 package mylib
 
 import (
-	"fmt"
+	"github.com/sirupsen/logrus"
 	"time"
 )
 
 func ShowCurrentTime() {
 	t := time.Now().Format("2006.01.02-15.04.05")
-	fmt.Println(t)
+	logrus.WithField("time", t).Info("current time")
 }
